70a8ced7556d2a0f4181187970154e26e2f152ba
One Fluffy Kiriko <danko.gakman@gmail.com>
2025-06-05 14:02:09 +0300

    Cleaned up the chatgpt code.

--- Labwork 4 Kiriko Edition/Labwork 4 Ex4.cpp
+++ Labwork 4 Kiriko Edition/Labwork 4 Ex4.cpp
@@ -1,22 +1,26 @@
 #include "main.h"
-#include <iomanip>
 using namespace std;
 int task4() {
-    const double PI = 3.1415;
-    double a = -PI / 2;
-    double b = 3 * PI / 2;
-    double dx = PI / 3;
-
-    cout << fixed << setprecision(4);
+    //The latter code was written by chatGPT, and adapted to use the rounding program included in the other source file.
+    //
+    const float PI = 3.1415;
+    float a = -PI / 2;
+    float b = 3 * PI / 2;
+    float dx = PI / 3;
     cout << "  x\t\t y = cos^2(x) + 3sin(x)\n";
     cout << "-------------------------------\n";
 
-    for (double x = a; x <= b + 1e-6; x += dx) {  // Add a small epsilon to ensure inclusion of b
-        double y = std::pow(std::cos(x), 2) + 3 * std::sin(x);
-        std::cout << x << "\t " << y << "\n";
+    for (double x = a; x <= b; x += dx) {  // **Add a small epsilon to ensure inclusion of b
+        double y = pow(cos(x), 2) + 3 * sin(x);
+        cout << x << "\t" << y << endl;
     }
 
     return 0;
 }
 
+
         
+
+
+
+
