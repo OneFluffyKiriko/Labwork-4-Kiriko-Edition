d982b76db938d8e1b197fab0fa0b3acaee653d92
One Fluffy Kiriko <danko.gakman@gmail.com>
2025-06-17 22:30:22 +0300

    Fixed up ex2

--- Labwork 4 Kiriko Edition/Labwork 4 Ex2.cpp
+++ Labwork 4 Kiriko Edition/Labwork 4 Ex2.cpp
@@ -7,28 +7,35 @@ using namespace std;
 // 7. Обчислити суму квадратів всіх парних чисел у діапазоні [1;20].
 int task2() {
 	cout << "\nStarting Exercise 2\n";
-	//Excercise A, a simple for loop.
+	
+	//Excercise 1, Solution A, a simple for loop.
 	cout << "Solution A! o((>w< ))o\n A very fancy and complicated for loop:\n";
+
 	int sum1 = 0, sum2 = 0, sum3 = 0;
 	for (int i = 1; i <= 20; i++) {
 		if (i % 2 == 0)
 			sum1 = sum1 + pow(i, 2);
 	}
 	cout << "sum of all even numbers squared from 1-20 is: " << sum1 << endl;
+	
 	//Solution B, a simple while loop.
 	cout << "Solution B! o((>w< ))o\n A very fancy and complicated while loop:\n";
-	int sqr = 0;
-	while (sqr <= 20) {
-		sqr++;
-		cout << sqr << " Squared is " << pow(sqr, 2) << endl;
+	int con1 = 0;
+	while (con1 <= 20) {
+		con1++;
+		if (con1 =)
+		sum2 = sum2 + pow(con1, 2);
+
 	}
+	cout << "sum of all even numbers squared from 1-20 is: " << sum2 << endl;
 	//Solution C, a simple do while loop.
 	cout << "Solution B! o((>w< ))o\n A very fancy and complicated do while loop:\n";
-	int sqr0 = 0;
+	int con2 = 0;
 	do {
-		sqr0++;
-		cout << sqr0 << " Squared is " << pow(sqr0, 2) << endl;
-	} while (sqr0 <= 20);
+		con2++;
+		sum3 = sum3 + pow(con2, 2);
+	} while (con2 <= 20);
+	cout << "sum of all even numbers squared from 1-20 is: " << sum1 << endl;
 
 	return 0;
 }
